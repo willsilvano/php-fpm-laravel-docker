de4345f1b78dd861a547a9f1f7b6943e44cc14cf
Willian <willsilvano@gmail.com>
2020-10-20 21:57:17 -0300

    Custom FFPM

--- application/app/Providers/RouteServiceProvider.php
+++ application/app/Providers/RouteServiceProvider.php
@@ -45,6 +45,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->mapApiRoutes();
 
         $this->mapWebRoutes();
+        $this->mapBotRoutes();
 
         //
     }
@@ -76,5 +77,19 @@ class RouteServiceProvider extends ServiceProvider
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapBotRoutes()
+    {
+        Route::middleware('bot')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/bot.php'));
     }
 }
